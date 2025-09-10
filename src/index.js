c55bc0eb4d46020e03602ec81ff8f38d4746d571
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-09-10 23:01:52 +0800

    hard reset for the logic -- try different approach tomorrow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 import "./input.css";
-import ocInputModal from "./js-configs/ocInputModal";
-import createToDoCard from "./js-configs/createTodoCard";
-import getFormValues from "./js-configs/getFormValues";
-
-ocInputModal("#inputDialog", "#todo-form", "#new-todo", "#cancelAddToDo");
-
--- src/js-configs/createTodoCard.js
+++
@@ -1,7 +0,0 @@
-import getFormValues from "./getFormValues";
-
-export default function createToDoCard() {
-
-    const formData = getFormValues();
-    return console.log(getFormValues());
-};
